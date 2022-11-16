07c7977a128a54878208bd753a06fb45d910b653
lgmf <lgm.ferrari@gmail.com>
2022-11-16 08:11:16 -0300

    fix grid size input

--- App.tsx
+++ App.tsx
@@ -4,31 +4,33 @@ import './style.css';
 import Board from './components/Board';
 
 export default function App() {
-  const [gridSize, setGridSize] = React.useState<number>(2);
+  const [inputValue, setInputValue] = React.useState('2');
+
+  const gridSize = Number(inputValue);
 
   return (
     <div className="app">
       <div className="field">
-        <label htmlFor="grid-size">Grid size:</label>
+        <label htmlFor="grid-size">Grid Size (even)</label>
         <input
           id="grid-size"
           type="number"
           min="2"
           step="2"
           max="100"
-          value={gridSize}
+          value={inputValue}
           onChange={(event) => {
             const value = Number(event.target.value);
-            const isValid = value && !Number.isNaN(value) && value % 2 === 0;
+            const isValid = !Number.isNaN(value) && value % 2 === 0;
 
             if (isValid) {
-              setGridSize(value);
+              setInputValue(event.target.value);
             }
           }}
         />
       </div>
 
-      {gridSize ? <Board gridSize={Number(gridSize)} /> : null}
+      {gridSize ? <Board gridSize={gridSize} /> : null}
     </div>
   );
 }
